e60778d903da3257799d88f157419ce78200f747
Trumkhungbo <25021981@vnu.edu.vn>
2026-04-10 15:17:41 +0700

    Create Entity.java

---
+++ user/Entity.java
@@ -0,0 +1,24 @@
+package user;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class Entity {
+    // Thuộc tính private (Encapsulation)
+    private String id;
+    private LocalDateTime createdAt;
+
+    public Entity() {
+        // Tự động sinh ID duy nhất và lấy thời gian hiện tại khi khởi tạo
+        this.id = UUID.randomUUID().toString(); 
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // Chỉ cung cấp Getter, không cung cấp Setter để bảo vệ dữ liệu gốc
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
