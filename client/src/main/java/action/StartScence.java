4c04a5216ca0c81447c27d3146b86da47331be3e
Trumkhungbo <25021981@vnu.edu.vn>
2026-05-08 01:04:33 +0700

    siêu khổ tâm

--- client/src/main/java/action/StartScence.java
+++ client/src/main/java/action/StartScence.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class StartScence extends Application {
+    public static Client client;
     @Override
     public void start(Stage primaryStage) throws Exception {
     FXMLLoader fxmloader = new FXMLLoader(StartScence.class.getResource("/views/login.fxml"));
@@ -18,4 +19,22 @@ public class StartScence extends Application {
     stage.show();
     Image icon = new Image(StartScence.class.getResourceAsStream("/assets/Icon.png"));
     stage.getIcons().add(icon);
-}}
+}
+
+        // Sửa lại đoạn này ở cuối file của bạn
+    public static class Client { // Chuyển thành public để class khác dùng được
+
+        public Client() {
+        }
+
+        // Thêm hàm này vào để hết lỗi "cannot find symbol"
+        public void sendMessage(String message) {
+            // Tạm thời để trống hoặc in ra console để kiểm tra
+            System.out.println("Gửi dữ liệu: " + message);
+            
+            // Sau này bạn sẽ thêm logic kết nối Socket vào đây
+        }
+    }
+
+    }
+
