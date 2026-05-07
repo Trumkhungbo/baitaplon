ba248433ef0a07dbaf49186534675ca1f5836125
Izdz-Ctrl <izdzmikokomi@gmail.com>
2026-05-07 22:38:26 +0700

    Release

--- client/src/main/java/action/StartScence.java
+++ client/src/main/java/action/StartScence.java
@@ -1,17 +1,17 @@
 package action;
 
+import com.auction.client.network.SocketClient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StartScence extends Application {
+    public static final SocketClient client = new SocketClient();
     @Override
     public void start(Stage primaryStage) throws Exception {
+        client.connect("127.0.0.1", 888);
     FXMLLoader fxmloader = new FXMLLoader(StartScence.class.getResource("/login.fxml"));
     Scene scene = new Scene(fxmloader.load());
     Stage stage = new Stage();
